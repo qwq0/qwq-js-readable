122034c3b32ba3c92efd0dd712ac8babe458c8a0
qwq0 <82009862+qwq0@users.noreply.github.com>
2023-10-24 02:11:27 +0800

    ud

--- src/processingCode.js
+++ src/processingCode.js
@@ -41,7 +41,7 @@ async function processingTree(tree)
 
     acornWalk.fullAncestor(tree, (now, state, ancestor) =>
     {
-        if (now.type == "ExpressionStatement" && now["expression"]) // 可替换的语句
+        if (now.type == "ExpressionStatement" && now["expression"]) // 表达式语句
         {
             let expression = now["expression"];
 
@@ -49,16 +49,18 @@ async function processingTree(tree)
             {
                 delete now["expression"];
 
-                now.type = "IfStatement";
-                now["test"] = expression.test;
-                now["consequent"] = {
-                    type: "ExpressionStatement",
-                    expression: expression.consequent
-                };
-                now["alternate"] = {
-                    type: "ExpressionStatement",
-                    expression: expression.alternate
-                };
+                Object.assign(now, {
+                    type: "IfStatement",
+                    test: expression.test,
+                    consequent: {
+                        type: "ExpressionStatement",
+                        expression: expression.consequent
+                    },
+                    alternate: {
+                        type: "ExpressionStatement",
+                        expression: expression.alternate
+                    }
+                });
 
                 doWork = true;
             }
@@ -69,22 +71,24 @@ async function processingTree(tree)
             {
                 delete now["expression"];
 
-                now.type = "IfStatement";
-                now["test"] = (
-                    expression.operator == "&&" ?
-                        expression.left :
-                        ({
-                            type: "UnaryExpression",
-                            operator: "!",
-                            prefix: true,
-                            argument: expression.left
-                        })
-                );
-                now["consequent"] = {
-                    type: "ExpressionStatement",
-                    expression: expression.right
-                };
-                now["alternate"] = null;
+                Object.assign(now, {
+                    type: "IfStatement",
+                    test: (
+                        expression.operator == "&&" ?
+                            expression.left :
+                            ({
+                                type: "UnaryExpression",
+                                operator: "!",
+                                prefix: true,
+                                argument: expression.left
+                            })
+                    ),
+                    consequent: {
+                        type: "ExpressionStatement",
+                        expression: expression.right
+                    },
+                    alternate: null
+                });
 
                 doWork = true;
             }
@@ -94,15 +98,117 @@ async function processingTree(tree)
             {
                 delete now["expression"];
 
-                now.type = "BlockStatement";
-                now["body"] = expression.expressions.map((/** @type {Object} */ o) =>
-                {
-                    return {
-                        type: "ExpressionStatement",
-                        expression: o
-                    };
+                Object.assign(now, {
+                    type: "BlockStatement",
+                    body: expression.expressions.map((/** @type {Object} */ o) =>
+                    {
+                        return {
+                            type: "ExpressionStatement",
+                            expression: o
+                        };
+                    })
                 });
 
+                doWork = true;
+            }
+            else if (
+                expression.type == "AssignmentExpression"
+            ) // 赋值运算符
+            {
+                let rightValue = expression.right;
+                if (rightValue.type == "ConditionalExpression") // 赋值套三目运算
+                {
+                    delete now["expression"];
+
+                    now.type = "";
+
+
+                    Object.assign(now, {
+                        type: "IfStatement",
+                        test: rightValue.test,
+                        consequent: {
+                            type: "ExpressionStatement",
+                            expression: {
+                                type: "AssignmentExpression",
+                                operator: expression.operator,
+                                left: structuredClone(expression.left),
+                                right: rightValue.consequent
+                            }
+                        },
+                        alternate: {
+                            type: "ExpressionStatement",
+                            expression: {
+                                type: "AssignmentExpression",
+                                operator: expression.operator,
+                                left: structuredClone(expression.left),
+                                right: rightValue.alternate
+                            }
+                        }
+                    });
+
+                    doWork = true;
+                }
+                else if (rightValue.type == "SequenceExpression") // 赋值套逗号运算符
+                {
+                    delete now["expression"];
+
+                    Object.assign(now, {
+                        type: "BlockStatement",
+                        body: [
+                            ...rightValue.expressions.slice(0, -1).map((/** @type {Object} */ o) =>
+                            {
+                                return {
+                                    type: "ExpressionStatement",
+                                    expression: o
+                                };
+                            }),
+                            {
+                                type: "ExpressionStatement",
+                                expression: {
+                                    type: "AssignmentExpression",
+                                    operator: expression.operator,
+                                    left: structuredClone(expression.left),
+                                    right: rightValue.expressions[rightValue.expressions.length - 1]
+                                }
+                            }
+                        ]
+                    });
+
+                    doWork = true;
+                }
+            }
+        }
+        else if (now.type == "IfStatement" && now["test"]) // if语句
+        {
+            let testExpression = now["test"];
+
+            if (
+                testExpression.type == "SequenceExpression"
+            ) // if语句条件中包含逗号运算符
+            {
+                delete now["expression"];
+
+                Object.assign(now, {
+                    type: "BlockStatement",
+                    body: [
+                        ...testExpression.expressions.slice(0, -1).map((/** @type {Object} */ o) =>
+                        {
+                            return {
+                                type: "ExpressionStatement",
+                                expression: o
+                            };
+                        }),
+                        {
+                            type: "ExpressionStatement",
+                            expression: {
+                                type: "IfStatement",
+                                test: testExpression.expressions[testExpression.expressions.length - 1],
+                                consequent: now["consequent"],
+                                alternate: now["alternate"]
+                            }
+                        }
+                    ]
+                });
                 doWork = true;
             }
         }
@@ -155,10 +261,10 @@ async function treeToCode(tree, moduleMode)
             "brace-style": ["error", "allman"],
             "nonblock-statement-body-position": ["error", "below"],
 
-            "no-lonely-if": ["error", true]
+            "no-lonely-if": ["error", true],
+            curly: ["error", "multi-or-nest"]
         }
-    }, {
-    });
+    }, {});
     if (lintResult.messages.length != 0)
         console.log("lint error:", lintResult.messages.map(o => `${o.message}\n${o.line}:${o.column}\n`).join("\n"));
 
